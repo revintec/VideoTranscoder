b8b42b136e03262562bc99b8b3e712f0e8dd4372
revintec <caoyunbin001@126.com>
2015-01-05 22:13:50 +0800

    revert OS identification, all OSes uses libfdk_aac now

--- src/com/revin/util/VideoTranscoder.java
+++ src/com/revin/util/VideoTranscoder.java
@@ -225,20 +225,8 @@ public class VideoTranscoder{
   }
   protected static final Pattern ptStat=Pattern.compile("fps=\\s*(\\d+).*time=([\\d:\\.]+)");
   protected static final Pattern ptTime=Pattern.compile("(\\d+):(\\d+):(\\d+)\\.(\\d+)");
-  public static final String vcodec,acodec;
-  public static final String[]ffmpeg;
-  static{
-    String ff;
-    if("Windows 8".equals(System.getProperty("os.name"))){
-      ff="ffmpeg -i PATH_IN -n -f mp4 -vcodec VCODEC -s SCALE -acodec ACODEC PATH_OUT";
-      vcodec="libx264";
-      acodec="libvo_aacenc";
-    }else{
-      ff="ffmpeg -i PATH_IN -n -f mp4 -vcodec VCODEC -s SCALE -acodec ACODEC -profile:a aac_he_v2 PATH_OUT";
-      vcodec="libx264";
-      acodec="libfdk_aac";
-    }ffmpeg=ff.split("\\s+");
-  }
+  public static final String vcodec="libx264",acodec="libfdk_aac";
+  public static final String[]ffmpeg="ffmpeg -i PATH_IN -n -f mp4 -vcodec VCODEC -s SCALE -acodec ACODEC -profile:a aac_he_v2 PATH_OUT".split("\\s+");
   public static final int ffmpegVcodec=index(ffmpeg,"VCODEC"),ffmpegAcodec=index(ffmpeg,"ACODEC");
   public static final int ffmpegIN=index(ffmpeg,"PATH_IN"),ffmpegSCALE=index(ffmpeg,"SCALE"),ffmpegOUT=index(ffmpeg,"PATH_OUT");
   public static final String transcodedPostfix=".mp4";
