f340d66ce873b929f20933b603aec901429db3d2
revintec <caoyunbin001@126.com>
2015-01-14 22:44:45 +0800

    some testing, not important

--- src/com/revin/util/Test.java
+++ src/com/revin/util/Test.java
@@ -1,14 +1,19 @@
 package com.revin.util;
 
-import java.util.Date;
-import java.util.Locale;
-
 /**
  * Created by revin on Dec.29,2014.
  */
 public class Test{
+    protected static boolean a=true;
+    public static boolean c(){
+        try{
+            return a;
+        }finally{
+           a=false;
+        }
+    }
     public static void main(String[] args){
-        Locale.setDefault(Locale.CHINA);
-        System.out.println(new Date());
+        System.out.println(c());
+        System.out.println(c());
     }
 }
